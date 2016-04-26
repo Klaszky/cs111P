93c19083631dbd5c95baec77f7910c825135809b
Joseph <jfklaszky@gmail.com>
2016-04-26 16:19:30 -0400

    update

--- WebReg.java
+++ WebReg.java
@@ -48,16 +48,51 @@ public class WebReg
 
 	public static int countCredits(Student s)
 	{
-		return 0;
+		int total = 0;
+
+		for(Course c : s.getSchedule())
+		{
+			if(c != null)
+				total += c.getCredits();
+		}
+
+		return total;
 	}
 
 	public static boolean addCourse(Student s, Course c)
 	{
-		return true;
+		if(isFullS(s) || isFullR(c))
+			
 	}
 
 	public static boolean dropCourse(Student s, Course c)
 	{
 		return true; 
+	}
+
+	public static boolean isFullS(Student s)
+	{
+		for(Course c : s.getSchedule())
+		{
+			if(c == null)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static boolean isFullR(Course c)
+	{
+		for(Student s : c.getRoster())
+		{
+			if(s == null)
+			{
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
